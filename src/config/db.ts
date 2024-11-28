e86ea5fde6eff05ca655925441a51733694405d2
Denis H-c <denishc0404@gmail.com>
2024-11-28 14:08:26 -0500

    feature deploy

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,11 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
+  user: "nbp_owner",
+  password: "ucGzJ4ywsjO1",
+  host: "ep-falling-union-a4n7a7y7.us-east-1.aws.neon.tech",
+  port: 5432,
+  database: "nbp",
+  ssl: true
 });
 
 export default pool;
